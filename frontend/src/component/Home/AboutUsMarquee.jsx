99c1bcd0308663d0b62a143c1d939f9f562eea9c
siddikgitt <siddikkhanpathan4@gmail.com>
2023-10-02 20:18:49 +0530

    text animation B2T scrolltrigger

--- frontend/src/component/Home/AboutUsMarquee.jsx
+++ frontend/src/component/Home/AboutUsMarquee.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import aboutImage from "../../assets/temp/about_HomePage.svg";
 import gsap from "gsap";
 import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { GSDevTools } from "gsap/GSDevTools";
 import Marquee from "react-fast-marquee";
 import ReactPlayer from "react-player";
@@ -12,15 +13,25 @@ import FontFaceObserver from 'fontfaceobserver';
 import g42Video from "../../assets/temp/Video/G42 Video.mp4";
 
 const AboutUsMarquee = () => {
-  gsap.registerPlugin(GSDevTools, SplitText);
+  gsap.registerPlugin(GSDevTools, SplitText, ScrollTrigger);
 
   const [isVisible, setIsVisible] = useState(false);
+  
+  const sectionRef1 = useRef(null);
+  const sectionRef2 = useRef(null);
+  
 
   useEffect(() => {
     gsap.registerPlugin(SplitText);
 
     let split;
     let tl;
+
+    let split2;
+    let t2;
+
+    const sectionElement1 = sectionRef1.current;
+  const sectionElement2 = sectionRef2.current;
 
     function init() {
       gsap.set(".fullScreen", { autoAlpha: 1 });
@@ -33,16 +44,56 @@ const AboutUsMarquee = () => {
         charsClass: "chars",
         linesClass: "lines",
       });
-      tl = gsap.timeline();
+      tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: sectionElement1,
+          start: 'bottom center+=50%', // Adjust this as needed
+          // end: 'bottom center', // Adjust this as needed
+          scrub: false, // Smoothly transition the animation
+          markers: false, // Remove this in production
+        },
+      });
 
       split.lines.forEach((line, index) => {
         const chars = line.querySelectorAll(".chars");
         tl.from(
           chars,
-          { duration: 0.3, yPercent: 100, stagger: 0.01, opacity: 0 },
+          { duration: 0.3, yPercent: 75, stagger: 0.007, opacity: 0 },
           ">-40%"
         );
-        tl.to(chars, { duration: 0.3, yPercent: 5, stagger: 0.01 }, ">-40%");
+        tl.to(chars, { duration: 0.3, yPercent: 7, stagger: 0.007 }, ">-40%");
+      });
+
+      // text2
+      gsap.set(".aboutUsText2", { autoAlpha: 1 });
+
+      if (split2) {
+        split2.revert();
+      }
+
+      split2 = new SplitText(".text2", {
+        charsClass: "chars",
+        linesClass: "lines",
+      });
+
+      t2 = gsap.timeline({
+        scrollTrigger: {
+          trigger: sectionElement2,
+          start: 'bottom center+=50%',
+          // end: 'bottom center',
+          scrub: false, // Smoothly transition the animation
+          markers: false, // Remove this in production
+        },
+      });
+
+      split2.lines.forEach((line, index) => {
+        const chars2 = line.querySelectorAll(".chars");
+        t2.from(
+          chars2,
+          { duration: 0.3, yPercent: 75, stagger: 0.007, opacity: 0 },
+          ">-40%"
+        );
+        t2.to(chars2, { duration: 0.3, yPercent: 7, stagger: 0.007 }, ">-40%");
       });
     }
 
@@ -67,6 +118,16 @@ const AboutUsMarquee = () => {
       "resize",
       debounce(() => {
         gsap.set(".fullScreen", { autoAlpha: 0 });
+        clearTimeout(timeout);
+        timeout = setTimeout(init, delay);
+      }, 250)
+    );
+
+     // Window resize event listener with debounce
+     window.addEventListener(
+      "resize",
+      debounce(() => {
+        gsap.set(".aboutUsText2", { autoAlpha: 0 });
         clearTimeout(timeout);
         timeout = setTimeout(init, delay);
       }, 250)
@@ -156,6 +217,7 @@ const AboutUsMarquee = () => {
           >
             <Box
               w={"100%"}
+              ref={sectionRef1}
               className={`fullScreen demo ${isVisible ? "visible" : "hidden"}`}
             >
               <Text
@@ -167,7 +229,7 @@ const AboutUsMarquee = () => {
                 color={"#747272"}
                 letterSpacing={"-0.42px"}
                 className="title"
-                onClick={toggleVisibility}
+                // onClick={toggleVisibility}
               >
                 About G42 Healthcare in 2 paragraphs not more than 60-70 words.
                 uses AI and data to create a world-class healthcare sector in
@@ -176,6 +238,11 @@ const AboutUsMarquee = () => {
               </Text>
             </Box>
 
+            <Box
+              w={"100%"}
+              ref={sectionRef2}
+              className={`aboutUsText2 demo ${isVisible ? "visible" : "hidden"}`}
+            >
             <Text
               mt={["15px"]}
               fontFamily={"Bossa-Light"}
@@ -185,14 +252,15 @@ const AboutUsMarquee = () => {
               lineHeight={"24px"}
               color={"#747272"}
               letterSpacing={"-0.42px"}
-              visibility={"hidden"}
-              className="aboutUsText2 demo2"
+              // visibility={"hidden"}
+              className="text2"
             >
               We built Biogenix Labs, the first COVID-19 accredited large-scale
               throughput lab in the UAE, and facilitated the world's first phase
               three inactivated COVID-19 vaccine trial, 4Humanity, with over
               43,000 participants from 125+ nationalities.
             </Text>
+            </Box>
             <Link href="/about">
               <Button
                 fontFamily={"Bossa-ExtendedMedium"}
